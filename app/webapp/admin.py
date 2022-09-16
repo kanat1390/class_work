aa80c95778b2ffaf7dfdb26afcf076c905e4ef57
Kanat <65421757+kanat1390@users.noreply.github.com>
2022-09-16 21:51:48 +0600

    Update admin.py

--- app/webapp/admin.py
+++ app/webapp/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Article
 
-# Register your models here.
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'author', 'created_at')
+    list_filter = ('author', 'created_at')
+    search_fields = ('title', 'author')
+    fields = ('title', 'author', 'created_at', 'text')
+    readonly_fields = ('id','created_at', 'changed_at')
+
+
+admin.site.register(Article, ArticleAdmin)
